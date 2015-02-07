b10a102d74fc376e96926748260864dfd73cb555
QuocHoi <quochoi@QuocHois-MacBook-Pro.local>
2015-02-08 00:51:54 +0700

    refs change editor html

--- iShop/mysite/_config.php
+++ iShop/mysite/_config.php
@@ -3,15 +3,10 @@
 global $project;
 $project = 'mysite';
 
-global $databaseConfig;
-$databaseConfig = array(
-	"type" => 'MySQLDatabase',
-	"server" => 'localhost',
-	"username" => 'root',
-	"password" => 'root@database',
-	"database" => 'SS_iShop_v319',
-	"path" => '',
-);
+global $database;
+$database = '';
+
+require_once('conf/ConfigureFromEnv.php');
 
 // Set the site locale
 i18n::set_locale('en_US');
